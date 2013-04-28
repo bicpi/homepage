1257a7f76457565b35a1cdfe095d7668ec50312d
Philipp Rieber <p.rieber@gmail.com>
2013-04-28 20:15:04 +0200

    implement silex improve layout add twitter widget add skill cloud add robots.txt + humans.txt

---
+++ web/index.php
@@ -0,0 +1,142 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+function shuffle_assoc(&$array) {
+    $keys = array_keys($array);
+
+    shuffle($keys);
+
+    foreach($keys as $key) {
+        $new[$key] = $array[$key];
+    }
+
+    $array = $new;
+
+    return true;
+}
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(
+    new Silex\Provider\TwigServiceProvider(),
+    array(
+        'twig.path' => dirname(__DIR__) . '/src/views',
+        'twig.form.templates' => array('form_div_layout.html.twig', 'form_layout.twig')
+    )
+);
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(
+    new Silex\Provider\TranslationServiceProvider(),
+    array(
+        'locale' => 'de',
+    )
+);
+$app->register(new Silex\Provider\FormServiceProvider());
+
+$app->before(
+    function () use ($app) {
+        $app['translator']->addLoader('xlf', new Symfony\Component\Translation\Loader\XliffFileLoader());
+        $app['translator']->addResource(
+            'xlf',
+            dirname(
+                __DIR__
+            ) . '/vendor/symfony/validator/Symfony/Component/Validator/Resources/translations/validators.de.xlf',
+            'de',
+            'validators'
+        );
+    }
+);
+
+$app->match(
+    '/',
+    function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
+        $form = $app['form.factory']->createBuilder('form')
+            ->add(
+                'name',
+                'text',
+                array(
+                    'constraints' => array(
+                        new Symfony\Component\Validator\Constraints\NotBlank(array('message' => 'Bitte gib Deinen Namen ein')),
+                        new Symfony\Component\Validator\Constraints\Length(array(
+                            'min' => 2,
+                            'minMessage' => 'Bitte gib mindestens {{ limit }} Zeichen ein'
+                        )),
+                    )
+                )
+            )
+            ->add(
+                'email',
+                'email',
+                array(
+                    'constraints' => array(
+                        new Symfony\Component\Validator\Constraints\NotBlank(array('message' => 'Bitte gib Deine E-Mail-Adresse ein')),
+                        new Symfony\Component\Validator\Constraints\Email(array('message' => 'Bitte gib eine gültige E-Mail-Adresse ein')),
+                    )
+                )
+            )
+            ->add(
+                'message',
+                'textarea',
+                array(
+                    'constraints' => array(
+                        new Symfony\Component\Validator\Constraints\NotBlank(array('message' => 'Bitte gib eine Nachricht ein')),
+                        new Symfony\Component\Validator\Constraints\Length(array(
+                            'min' => 10,
+                            'minMessage' => 'Bitte gib mindestens {{ limit }} Zeichen ein'
+                        ))
+                    )
+                )
+            )
+            ->getForm();
+
+        if ('POST' == $request->getMethod()) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $data = $form->getData();
+
+                $message = \Swift_Message::newInstance('Nachricht von der Homepage')
+                    ->setFrom(array('info@philipp-rieber.net' => 'philipp-rieber.net'))
+                    ->setTo('info@philipp-rieber.net')
+                    ->setBody(
+                        sprintf(
+                            "Homepage-Nachricht von %s <%s>\n\n\n%s",
+                            $data['name'],
+                            $data['email'],
+                            $data['message']
+                        )
+                    );
+                $app['mailer']->send($message);
+
+                $app['session']->getFlashBag()->add(
+                    'success',
+                    sprintf(
+                        '<b>%s</b>, vielen Dank für Deine Nachricht.',
+                        $app->escape($data['name'])
+                    )
+                );
+
+                return $app->redirect('/#');
+            }
+        }
+
+        $skillsRaw = \Symfony\Component\Yaml\Yaml::parse(dirname(__DIR__).'/src/config/skills.yml');
+        $skills = array();
+        foreach ($skillsRaw as $weight => $skillGroup) {
+            foreach ($skillGroup as $skill) {
+                $skills[$skill] = $weight;
+            }
+        }
+
+        shuffle_assoc($skills);
+
+        return $app['twig']->render('home.twig', array('skills' => $skills, 'form' => $form->createView()));
+    }
+)
+    ->method('GET|POST');;
+
+$app->run();
